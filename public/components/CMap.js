686c81e3c2cfdb9ecb490e72fec981c4dc011749
Lat-ta <laetitia.carvalho@gmail.com>
2021-01-18 17:19:21 +0100

    cleaning

--- public/components/CMap.js
+++ public/components/CMap.js
@@ -2,10 +2,6 @@ class Map
 {
     constructor(mapId){
         this.map = L.map(mapId).setView([45.756104, 4.841173], 12);
-
-        // var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        // });
         var osmLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
@@ -25,16 +21,35 @@ class Map
         function markerOnClick(evt)
         {
             // console.log('plop'+evt.latlng);
-            sidebar.setContent('Informations du Spot :'+'<br> <br>'+evt.latlng+'<br> <br>'+JSON.stringify(evt.target.myJsonData));
+            var content ='Informations du Spot :'+'<br> <br>'+evt.latlng+'<br> <br>';
+            //+JSON.stringify(evt.target.myJsonData)
+
+            var obj = evt.target.myJsonData;
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    var val = obj[key];
+                    // console.log(key+'-'+val);
+                    content+='<strong>'+key+': </strong> '+val+'<br> <br>'
+                }
+            }
+
+            sidebar.setContent(content);
             // Mettre le formulaire de la sidebar pour créer un point 
             if (!sidebar.isVisible()) {
-            sidebar.toggle();
+                sidebar.toggle();
             }
         }
-
         
-        this.map.on('click', function () {
+        this.map.on('click', function (e) {
             sidebar.hide();
+            //Ajouter un marker à la BDD
+            // var mp = new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+            // // defineYourWaypointOnClick(e,this.map);
+            // var container = L.DomUtil.create('div');
+            // //////////////////////////////////////////////////////////////////////////////////////////////
+            // ///////////modified here
+            // var startBtn = createButton('Start from this location', container);
+            // var marker = new L.marker(e.latlng).bindPopup('You clicked the map at ' + e.latlng.toString() + '<br>' + startBtn).addTo(map);
         })
 
         sidebar.toggle();
@@ -50,15 +65,14 @@ class Map
                 success: function (markers_json) {
                     // console.log(markers_json)
                     markers_json.forEach(function(data){
-                        console.log(data);
-                        console.log(typeof(data));
+                        // console.log(data);
+                        // console.log(typeof(data));
                         var lat = data.gps.lat;
                         var lng = data.gps.lon;
                         var markers = L.marker([parseFloat(lat), parseFloat(lng)],{draggable: false,        // Make the icon dragable
                             title: data.title} )
                         .addTo(curmap).openPopup().on('click',markerOnClick).myJsonData = data;
                     });
-            
                 },
                 error: function (response, error) {
                     $("#coordonnees").html("Ca a pas marché Roger marker");
@@ -67,51 +81,11 @@ class Map
             });
         };
 
-
         getMarkers(this.map);
-
-        // //For test
-        // var ggRoadmap = new L.Google('ROADMAP');
-        // var ggSatellite = new L.Google('');
-        // var ggTerrain = new L.Google('TERRAIN');
-        // var ggHybrid = new L.Google('HYBRID');
 
         this.map.addLayer(osmLayer); // Le layer par défaut
 
-        // this.map.addControl(new L.Control.Layers( {
-        //     'OpenStreetMap': osmLayer, 
-        //     'Google Roadmap' : ggRoadmap, 
-        //     'Google Satellite': ggSatellite, 
-        //     'Google Terrain': ggTerrain, 
-        //     'Google Hybrid' : ggHybrid
-        //     }, {})
-        // );
-        // //End of test
-        // this.map = L.map(mapId).setView([45.756104, 4.841173], 12);
-        // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        //     maxZoom: 18,
-        //     id: 'mapbox/streets-v11',
-        //     tileSize: 512,
-        //     zoomOffset: -1,
-        //     accessToken: 'pk.eyJ1Ijoic2NhcmF1ZGUiLCJhIjoiY2tnYXJpdDh1MDl2NTJ4cnR3c2c4NjVzcSJ9.UkZLikOnXgNA-j0Dmoub3w'
-        // }).addTo(this.map);
 
-        // this.map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));	//base layer
-        function searchByAjax(val, callResponse)//callback for 3rd party ajax requests
-        {
-            return $.ajax({
-                url: 'https://geo.api.gouv.fr/communes?nom=' + val + '&fields=nom,centre,departement&boost=population&limit=5',
-                type: 'GET',
-                success: function(json) {
-                    callResponse(json);
-                }
-            });
-        }
-
-        this.map.addControl( new L.Control.Search({sourceData: searchByAjax, text:'Rechercher...', markerLocation: true,zoom: 12, marker: false}) );
-
-        
     } // End constructor 
 
     locate(){
