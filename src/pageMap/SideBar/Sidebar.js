7eea3b1e32fee86bde1818c91994fb3d1c15b08f
scaraude <ludodante@hotmail.fr>
2021-04-01 16:11:16 +0200

    Intégration de la SideBar

--- src/pageMap/SideBar/Sidebar.js
+++ src/pageMap/SideBar/Sidebar.js
@@ -2,25 +2,37 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ArrowRightAltRoundedIcon from "@material-ui/icons/ArrowRightAltRounded";
 import { Drawer } from "@material-ui/core";
-import IconButton from '@material-ui/core/IconButton';
+import IconButton from "@material-ui/core/IconButton";
 
-import RenderSpotInfos from "./RenderSpotInfos";
+import RenderSpotInfos from "./renders/RenderSpotInfos";
+import RenderSpotIconButtons from './renders/RenderSpotIconButtons';
 
 const StyledPanel = styled.div`
   width: 33vw;
-  padding: 1.5rem;
+  min-height: calc(100% - 1rem);
+  background-color: #ececec;
+  padding: 0.5rem;
+`;
+
+const StyledHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Sidebar = ({ open = false, spot = null, handleDrawerClose }) => {
   return (
     <Drawer anchor="right" open={open} onClose={() => handleDrawerClose()}>
       <StyledPanel>
-      <IconButton onClick={handleDrawerClose}>
-            <ChevronRightIcon />
-        </IconButton>
-       {spot && <RenderSpotInfos spot={spot}/>}
+        <StyledHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <ArrowRightAltRoundedIcon />
+          </IconButton>
+          {spot && <RenderSpotIconButtons/>}
+        </StyledHeader>
+        {spot && <RenderSpotInfos spot={spot} />}
       </StyledPanel>
     </Drawer>
   );
