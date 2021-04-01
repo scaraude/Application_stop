dc07b7e3d4d9318e14cef9f77f1f019d19e582a7
scaraude <ludodante@hotmail.fr>
2021-04-01 16:11:16 +0200

    Intégration de la SideBar

---
+++ src/pageMap/SideBar/renders/renderSpotIconButtons.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import CreateRoundedIcon from "@material-ui/icons/CreateRounded";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const RenderSpotIconButtons = () => (
+    <div>
+      <IconButton>
+        <FavoriteBorderIcon />
+      </IconButton>
+      <IconButton>
+        <CreateRoundedIcon />
+      </IconButton>
+      <IconButton>
+        <MoreVertIcon />
+      </IconButton>
+    </div>
+  )
+
+  export default RenderSpotIconButtons
