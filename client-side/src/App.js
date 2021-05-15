33cef71a2fca51bcab5d6178caf89df9304a4926
scaraude <ludodante@hotmail.fr>
2021-05-15 16:44:20 +0200

    profile page

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Map from "./pageMap/Map";
 import NavBar from "./NavBar/NavBar";
-import Login from './pageAuth/Login';
-import SignUp from './pageAuth/SignUp';
+import Login from "./pageAuth/Login";
+import SignUp from "./pageAuth/SignUp";
+import Profile from "./pageUser/Profile";
 
 const App = () => (
   <Router>
-    <NavBar/>
+    <NavBar />
     <Switch>
       <Route exact path="/">
         <Map />
@@ -18,6 +19,9 @@ const App = () => (
       </Route>
       <Route path="/signup">
         <SignUp />
+      </Route>
+      <Route path="/profile">
+        <Profile />
       </Route>
     </Switch>
   </Router>
