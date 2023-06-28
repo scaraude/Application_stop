c53449c02ec1ef34e4635947e992392c77b16fc1
scaraude <ludodante@hotmail.fr>
2023-06-28 00:05:22 -0400

    ignore eslint error

--- client-side/webpack.config.js
+++ client-side/webpack.config.js
@@ -1,66 +1,68 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 const path = require("path");
 
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  entry: "./client-side/src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "./public"),
-    filename: "./bundle.js",
-    sourceMapFilename: '[name].[hash:8].map',
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    historyApiFallback: true,
-    open: true,
-    host: "localhost",
-    hot: true,
-    proxy: [
-      {
-        context: ['/auth', '/api', '/login', '/signup'],
-        target: 'http://localhost:8080',
-      }
-    ],
-  },
-  plugins: [
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|jp2|webp)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-        }
-      },
-      { 
-        test: /\.css$/,use: ['style-loader', 'css-loader'] 
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    alias: {
-      "@utils": path.resolve(__dirname, 'packages/utils'),
-    },
-  },
+	entry: "./client-side/src/index.tsx",
+	output: {
+		path: path.resolve(__dirname, "./public"),
+		filename: "./bundle.js",
+		sourceMapFilename: "[name].[hash:8].map",
+	},
+	devtool: "eval-source-map",
+	devServer: {
+		historyApiFallback: true,
+		open: true,
+		host: "localhost",
+		hot: true,
+		proxy: [
+			{
+				context: ["/auth", "/api", "/login", "/signup"],
+				target: "http://localhost:8080",
+			}
+		],
+	},
+	plugins: [
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.(ts|tsx)$/i,
+				loader: "ts-loader",
+				exclude: ["/node_modules/"],
+			},
+			{
+				test: /\.(png|jpe?g|gif|jp2|webp)$/,
+				loader: "file-loader",
+				options: {
+					name: "[name].[ext]",
+				}
+			},
+			{ 
+				test: /\.css$/,use: ["style-loader", "css-loader"] 
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+		alias: {
+			"@utils": path.resolve(__dirname, "packages/utils"),
+		},
+	},
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
+	if (isProduction) {
+		config.mode = "production";
+	} else {
+		config.mode = "development";
+	}
+	return config;
 };
--- server-side/webpack.config.js
+++ server-side/webpack.config.js
@@ -1,50 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require("path");
-const nodeExternals = require('webpack-node-externals');
-const NodemonPlugin = require('nodemon-webpack-plugin');
+const nodeExternals = require("webpack-node-externals");
+const NodemonPlugin = require("nodemon-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  context: __dirname,
-  entry: "./src/server.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new NodemonPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: ["/node_modules/*"],
-        options: { configFile: "tsconfig.json" }
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-    ],
-  },
-  target: 'node',
-  externalsPresets: { node: true },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+	context: __dirname,
+	entry: "./src/server.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+	devServer: {
+		static: "./dist",
+	},
+	plugins: [
+		new NodemonPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: "ts-loader",
+				exclude: ["/node_modules/*"],
+				options: { configFile: "tsconfig.json" }
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: "asset",
+			},
+		],
+	},
+	target: "node",
+	externalsPresets: { node: true },
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
+	if (isProduction) {
+		config.mode = "production";
+	} else {
+		config.mode = "development";
+	}
+	return config;
 };
